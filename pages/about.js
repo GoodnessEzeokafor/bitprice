5680804554dc765057c6da72e91b55b8c5a4b6f2
Goodness Ezeokafor <gootech442@yahoo.com>
2020-08-31 22:46:21 +0100

    changes

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout"
+
+const About = () =>  (
+    <Layout>
+        <div>
+                <h1>About BitPrice</h1>
+                <p>Application To view Bitcoin Prices</p>
+        </div>
+    </Layout>
+)
+export default About 
---
+++ pages/contact.js
@@ -0,0 +1,13 @@
+
+import Layout from "../components/Layout"
+
+const Contact = () => (
+    <Layout>
+        <div>
+                <h1>Contact Us</h1>
+                <p>Fill In The Form</p>
+        </div>
+    </Layout>
+)
+
+export default Contact
